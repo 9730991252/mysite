fe3197ff110e318f50165e0e7858c59d08d4d8b9
9730991252 <sunilkale101@gmail.com>
2024-09-24 18:57:24 +0530

    ok

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-w(ebww$(l8h(!8b*6&li_vc0t_6niq^o10%_xyhd9*re*4llwk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
